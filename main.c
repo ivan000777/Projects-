0f3f08c71dace2ffef7718da4bb0963a0e672561
Мартинюк Іван <im.martyniuk@ukma.edu.ua>
2025-09-15 23:09:13 +0300

    практична два

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("|     Mo     |     Tue     |     We     |     Th     |     Fr     |     Sa     |     Su     |\n");
+    printf("______________________________________________________________________________________________\n");
+    printf("|            |             |            |            |            |            |            |\n");
+    printf("|8.30        |             |            |            |8.30        |            |            |\n");
+    printf("|Ukrainian   |             |9.30        |            |Math        |            |            |\n");
+    printf("|            |10.15        |IT          |            |            |            |            |\n");
+    printf("|            |Math         |            |            |11.00       |            |            |\n");
+    printf("|11.30       |             |11.30       |            |IT          |11.30       |            |\n");
+    printf("|Electronics |             |Ukrainian   |            |            |Ukrainian   |            |\n");
+    printf("|            |             |            |            |            |            |            |\n");
+    printf("|            |12.30        |            |12.30       |            |            |            |\n");
+    printf("|            |English      |            |English     |            |            |            |\n");
+    printf("______________________________________________________________________________________________\n");
+
+    return 0;
+}
